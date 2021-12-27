80073e6202c8d50a09cb57d74c0b37c5f2e045bd
Indra Kusumah-kasim <shadeditor@gmail.com>
2021-12-27 18:54:56 +1100

    idk what i did tbh

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="w-full min-h-screen flex flex-col items-center">
     <Bar />
+    <Block class="h-4/6 w-full flex-auto"/>
   </div>
 </template>
 
 <script>
 import Bar from './components/Bar'
+import Block from './components/Block'
 
 export default {
   name: 'App',
-  components: { Bar }
+  components: { Bar, Block }
 }
 </script>
 
@@ -20,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  @apply text-white;
+  @apply text-white text-lg;
 }
 </style>
