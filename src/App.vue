ad896e2403dc0bc5fbd7c04e28527265eee21eec
Indra Kusumah-kasim <shadeditor@gmail.com>
2021-12-27 18:08:32 +1100

    Created top bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="w-full min-h-screen flex flex-col items-center">
+    <Bar />
+  </div>
 </template>
 
 <script>
+import Bar from './components/Bar'
 
 export default {
   name: 'App',
+  components: { Bar }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  @apply text-white;
 }
 </style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-gradient-to-r from-blue-700 to-blue-900
+              flex-none w-full h-14 flex items-center">
+    <div class="text-left flex-auto">
+      <span class="pl-2 font-semibold text-3xl">Reaction Timer</span>
+    </div>
+    <div class="flex-none">
+      <img src="https://github.githubassets.com/favicons/favicon-dark.svg"
+           class="h-10 w-10 mr-2"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bar"
+}
+</script>
