e9fe92132c086459a18675dbf5fc9bce1d5e1b13
Indra Kusumah-kasim <shadeditor@gmail.com>
2021-12-28 12:28:53 +1100

    Changed title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vueReactiontimerProject/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Reaction Timer'
+        return args
+      })
+  }
 }
